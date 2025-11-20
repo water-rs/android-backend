7965ff6b9226fb0090755b0cb53f0f47517023b0
Lexo Liu <me@lexo.cool>
2025-11-20 18:10:39 +0800

    Bump Android backend to Java 21

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -29,12 +29,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
         freeCompilerArgs += listOf("-Xjvm-default=all")
     }
 
@@ -52,7 +52,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
